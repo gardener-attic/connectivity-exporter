f995162cdddacb1dde1b2fd18ca45b0a999dc3f5
Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>
2021-12-02 12:08:23 +0100

    Remove helper scripts of the connectivity-exporter

--- monitoring-stack/bin/node-port.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
-#
-# SPDX-License-Identifier: Apache-2.0
-
-kubectl -n istio-ingress get svc \
-        -o=jsonpath='{.items[0].spec.ports[?(@.name == "tcp")].nodePort}'
