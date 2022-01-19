68148da7255fc5e9ddda065af92469178443e76d
Wesley Bermbach <wesley.bermbach@sap.com>
2022-01-19 13:19:01 +0100

    Add kube-prometheus-stack installer script
    Configured to only deploy prometheus and grafana
    
    Co-authored-by: Wesley Bermbach <wesley.bermbach@sap.com>
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>

---
+++ hack/kube-prometheus-stack/up.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+cd "$(dirname "$(realpath "$0")")" || exit 1
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+helm upgrade --install \
+             --values values.yaml \
+             --version 30.0.1 \
+             --create-namespace \
+             --namespace monitoring \
+             kube-prometheus-stack \
+             prometheus-community/kube-prometheus-stack
